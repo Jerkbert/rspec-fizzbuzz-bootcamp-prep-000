ea26f1e8537533e970e07f7669647cefaa3059e8
Rob Ballantyne <raballantyne@gmail.com>
2019-07-12 21:37:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(n)
+def fizzbuzz(n) do
   if n % 3
     puts "Fizz"
   elsif n % 5
