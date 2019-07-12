6363274576ca7181fbd45723a09a1b0a9bdb287e
Rob Ballantyne <raballantyne@gmail.com>
2019-07-12 21:50:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(n)
-  if n % 3 == 0
+  ifn % 3 == 0 && n % 5 == 0
     "Fizz"
   elsif n % 5 == 0
     "Buzz"
-  else n % 3 == 0 && n % 5 == 0
+  else 
   "FizzBuzz"
   end
 end
