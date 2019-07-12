aa1b116ab7ecefaf31271285d7d127eeb4601017
Rob Ballantyne <raballantyne@gmail.com>
2019-07-12 21:38:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
 def fizzbuzz(n) do
   if n % 3
     puts "Fizz"
@@ -9,4 +10,4 @@ def fizzbuzz(n) do
   puts "FizzBuzz"
 else puts "nil"
 end
-
+end
